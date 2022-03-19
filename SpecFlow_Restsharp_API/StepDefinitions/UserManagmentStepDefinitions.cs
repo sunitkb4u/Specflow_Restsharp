738508ca9169b9016974391b56f8b9a818719211
Sunit <sunil.kamble2787@gmail.com>
2022-03-19 10:55:51 +0530

    Desktop Work

--- SpecFlow_Restsharp_API/StepDefinitions/UserManagmentStepDefinitions.cs
+++ SpecFlow_Restsharp_API/StepDefinitions/UserManagmentStepDefinitions.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework;
+using RestSharp;
+using SpecFlow_Restsharp_API.HelperClass;
 using System;
 using TechTalk.SpecFlow;
 
@@ -6,68 +9,26 @@ namespace SpecFlow_Restsharp_API.StepDefinitions
     [Binding]
     public class UserManagmentStepDefinitions
     {
+        string endPoint = "api/users/";
+        RestClient client;
+        RestRequest request;
+        RestResponse response;
+        RequestHelper requestHelper = new RequestHelper();
+
         [Given(@"User enters user id (.*)")]
         public void GivenUserEntersUserId(int id)
         {
-            throw new PendingStepException();
+            endPoint=endPoint+id;
         }
 
         [When(@"User send get user request")]
         public void WhenUserSendGetUserRequest()
         {
-            throw new PendingStepException();
+            requestHelper.SetUrl(TestContext)
         }
 
         [Then(@"User validates details (.*) (.*) and http response code (.*)")]
         public void ThenUserValidatesDetailsJanetWeaverAndHttpResponseCode(string firstName, string lastName, int httpCode)
-        {
-            throw new PendingStepException();
-        }
-
-        [Given(@"User entrs job ().*")]
-        public void GivenUserEntrsJobTester(string job)
-        {
-            throw new PendingStepException();
-        }
-
-        [Given(@"User enters role(.*)")]
-        public void GivenUserEntersRoleAPITest(string role)
-        {
-            throw new PendingStepException();
-        }
-
-        [When(@"User send create user request")]
-        public void WhenUserSendCreateUserRequest()
-        {
-            throw new PendingStepException();
-        }
-
-        [Then(@"user should get created")]
-        public void ThenUserShouldGetCreated()
-        {
-            throw new PendingStepException();
-        }
-
-        [Given(@"user want to update user id (.*)")]
-        public void GivenUserWantToUpdateUserId(int id)
-        {
-            throw new PendingStepException();
-        }
-
-        [When(@"User send update user request")]
-        public void WhenUserSendUpdateUserRequest()
-        {
-            throw new PendingStepException();
-        }
-
-        [Then(@"user should get updated")]
-        public void ThenUserShouldGetUpdated()
-        {
-            throw new PendingStepException();
-        }
-
-        [Given(@"User enters job user id (.*)")]
-        public void GivenUserEntrsJobUserId(int id)
         {
             throw new PendingStepException();
         }
@@ -78,7 +39,7 @@ namespace SpecFlow_Restsharp_API.StepDefinitions
             throw new PendingStepException();
         }
 
-        [Then(@"Validate iser should get deleted (.*)")]
+        [Then(@"Validate user should get deleted (.*)")]
         public void ThenValidateIserShouldGetDeletedHttpCode(int httpCode)
         {
             throw new PendingStepException();
